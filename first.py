3e9cd335beeb0bd2546833ed0909c673eab21eca
sabbir-sabbir <iosabbir7890@gmail.com>
2025-06-17 18:19:24 +0600

    Learning python

---
+++ first.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text, this is written by a programmer named Sabbir ")
+engine.runAndWait()
---
+++ type.py
@@ -0,0 +1,37 @@
+# a = input("Input Your First Number!:   ")
+# b = input("Input Your Second Number!:  ")
+# x = int(a)
+# y = int(b)
+# print("First Number is", x)
+# print("Second Number is", y)
+
+
+# totalOfTwoNumbers = x + y
+# print("Square of your two Numbers are!", totalOfTwoNumbers**2)
+
+
+# name = "Trenis"
+# nameShort = name[0:3] #seems like js slice method.
+# print(nameShort)
+
+# a = "123456789"
+# b = a[1:5:3]
+# print(b)
+
+# name = "Trenis"
+# b = len(name)
+# print(b)
+
+# x = name.startswith("T")
+# print(x)
+
+
+# smallStr = "sabbir rahman"
+# x = smallStr.capitalize()
+# print(x)
+
+
+# name = 'SABBIR\nRahma\'n\''
+# print(name)
+
+print("Name:\tSabbir")
