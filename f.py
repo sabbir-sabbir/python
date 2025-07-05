a16eb0e67e7a1c6a9454968eeb92e39ecc7ef3e5
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-05 19:31:12 +0600

    add

--- f.py
+++ f.py
@@ -1,13 +1,21 @@
-def avg():
-    a = 12
-    b = 13
-    c = 14
+# def avg():
+#     a = 12
+#     b = 13
+#     c = 14
 
-    average = (a+b+c)/3
-    print(average)
+#     average = (a+b+c)/3
+#     print(average)
 
-avg()
-print("Thank you")
-avg()
-print("Thank you")
-avg()
+# avg()
+# print("Thank you")
+# avg()
+# print("Thank you")
+# avg()
+
+
+
+def goodDay():
+    name = input("Enter your name get greet:  ")
+    print("Good Morning", name)
+
+goodDay()    
