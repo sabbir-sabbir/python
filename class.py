e4087e7478f1b76764b6f531f10eafc0c946dec8
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-06 21:47:55 +0600

    add

--- class.py
+++ class.py
@@ -1,4 +1,4 @@
-# class in python
+# class in python::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # instance vs class atributes
 # class Employee:
 #     name = "Threnis"
@@ -10,21 +10,21 @@
 # print(EmployeeNumber_1.name, EmployeeNumber_1.salary, EmployeeNumber_1.language)
 
 
-# self  parameter (We can modify it by any name)::
-class Employee:
-    name = "Threnis"
-    language = "German"
-    salary = 120000
+# self  parameter (We can modify it by any name)::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# class Employee:
+#     name = "Threnis"
+#     language = "German"
+#     salary = 120000
 
-    def getInfo(this):
-        print(f"The language is {this.language}. The salary is {this.salary}")
+#     def getInfo(this):
+#         print(f"The language is {this.language}. The salary is {this.salary}")
 
-    def greet(this):
-        print(f"Good morning {this.name}")     
+#     def greet(this):
+#         print(f"Good morning {this.name}")     
 
-employee_1 = Employee()  
-employee_1.getInfo()      
-employee_1.greet()      
+# employee_1 = Employee()  
+# employee_1.getInfo()      
+# employee_1.greet()      
 
 
 
