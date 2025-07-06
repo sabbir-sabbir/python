075cf0dc9ef10036ca836c7856e19730747a580b
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-06 21:40:34 +0600

    add

--- class.py
+++ class.py
@@ -1,9 +1,26 @@
 # class in python
+# instance vs class atributes
+# class Employee:
+#     name = "Threnis"
+#     language = "German"
+#     salary = 120000
+
+# EmployeeNumber_1 = Employee()
+# EmployeeNumber_1.name = "Threnis"
+# print(EmployeeNumber_1.name, EmployeeNumber_1.salary, EmployeeNumber_1.language)
+
+
+# self  parameter (We can modify it by any name)::
 class Employee:
     name = "Threnis"
     language = "German"
     salary = 120000
 
-EmployeeNumber_1 = Employee()
-EmployeeNumber_1.name = "Threnis"
-print(EmployeeNumber_1.name, EmployeeNumber_1.salary, EmployeeNumber_1.language)
+    def getInfo(this):
+        print(f"The language is {this.language}. The salary is {this.salary}")
+
+employee_1 = Employee()  
+Employee.getInfo(employee_1)      
+
+
+
