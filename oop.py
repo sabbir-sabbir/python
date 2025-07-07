4574135a1cfdbe6cd18b55c8e0a49c646dc76483
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-07 22:20:32 +0600

    Add

--- oop.py
+++ oop.py
@@ -177,4 +177,46 @@
 
 
 
+# class Number:
+#     def __init__(self, n):
+#         self.n = n
 
+#     def __add__(self, num) :
+#         return self.n + num.n   
+        
+
+
+# n = Number(12)
+# m = Number(13)
+
+# print(n + m)
+
+
+
+
+
+
+
+
+
+
+
+class TwoDVector:
+    def __init__(self, i, j):
+        self.i = i 
+        self.j = j
+    def show(self):
+        print(f"The vector is: {self.i}i + {self.j}j ")
+
+class ThreeDVector(TwoDVector):
+    def __init__(self, i, j, k):
+         super().__init__(i, j)
+         self.k = k
+
+    def show(self):
+        print(f"The vector is: {self.i}i + {self.j}j + {self.k}k ")
+
+a = TwoDVector(1, 2)
+a.show()
+b = ThreeDVector(1,2,3)
+b.show()
