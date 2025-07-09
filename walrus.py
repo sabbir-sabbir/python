581ec8a7d7ed10c4a64fb5c7667128e25e470064
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-09 19:11:04 +0600

    add

---
+++ walrus.py
@@ -0,0 +1,13 @@
+# def sum(a: int, b: int) -> int:
+#     return a + b
+
+
+# print(sum(5,6))
+from typing import Tuple, List, Dict, Union
+
+person: Tuple[str, int] = ("Alice", 30)
+scores: Dict[str, int] = {"SABBIR": 19, "Home": 717}
+identifier: Union[str, int] = "ID123"
+Alist: List[int] = [1,2,3,4,55,6]
+
+print(person, scores, identifier, Alist)
