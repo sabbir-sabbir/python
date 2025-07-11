a8b966e7f860430bcf836b0217c9cbb0bd1aff62
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-11 23:52:58 +0600

    add

--- walrus.py
+++ walrus.py
@@ -3,11 +3,21 @@
 
 
 # print(sum(5,6))
-from typing import Tuple, List, Dict, Union
+# from typing import Tuple, List, Dict, Union
 
-person: Tuple[str, int] = ("Alice", 30)
-scores: Dict[str, int] = {"SABBIR": 19, "Home": 717}
-identifier: Union[str, int] = "ID123"
-Alist: List[int] = [1,2,3,4,55,6]
+# person: Tuple[str, int] = ("Alice", 30)
+# scores: Dict[str, int] = {"SABBIR": 19, "Home": 717}
+# identifier: Union[str, int] = "ID123"
+# Alist: List[int] = [1,2,3,4,55,6]
 
-print(person, scores, identifier, Alist)
+# print(person, scores, identifier, Alist)
+
+
+try:
+    a = int(input("Hey, Enter a number:  "))
+    print(a)
+
+except Exception  as e:
+    print(e) 
+
+print("Thank you")
