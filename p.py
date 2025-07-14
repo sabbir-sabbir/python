62255deca977bd516ad792c9ed7aba2ed5c09c05
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-14 23:39:03 +0600

    # def FileCreationFunc(): #     if os.path.exists("1.txt"): #         with open("1.txt", "r") as f: #             print(f.read()) #     else: #         with open("1.txt", "w") as f: #             f.write("This file was just created because it didn't exist.\n") #         print("1.txt was not found, so it has been created with default content.")
    # FileCreationFunc()

--- p.py
+++ p.py
@@ -1,13 +1,26 @@
 import os
-
-def FileCreationFunc():
-    if os.path.exists("1.txt"):
-        with open("1.txt", "r") as f:
-            print(f.read())
-    else:
-        with open("1.txt", "w") as f:
-            f.write("This file was just created because it didn't exist.\n")
-        print("1.txt was not found, so it has been created with default content.")
+from typing import List
 
 
-FileCreationFunc()
+
+
+Mylist: List[int] = [1,2,3,4,5,6,7,8,9]
+
+for i, item in enumerate(Mylist):
+    if i == 2 or i == 4 or i == 6:
+        print(item)
+
+
+
+
+        # def FileCreationFunc():
+#     if os.path.exists("1.txt"):
+#         with open("1.txt", "r") as f:
+#             print(f.read())
+#     else:
+#         with open("1.txt", "w") as f:
+#             f.write("This file was just created because it didn't exist.\n")
+#         print("1.txt was not found, so it has been created with default content.")
+
+
+# FileCreationFunc()
