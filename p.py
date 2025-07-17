cb97fda0f07439cd2abdfb8409e725bb9eed1be4
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-17 23:31:48 +0600

    add

--- p.py
+++ p.py
@@ -1,10 +1,10 @@
-import os
-from typing import List
-from functools import reduce
+# import os
+# from typing import List
+# from functools import reduce
 
 
 
-# reduc.......................................................
+# reduce.......................................................
 # MyList: List[int] = [1,2,3,4,5,6,7,8]
 
 # def sum(a, b):
